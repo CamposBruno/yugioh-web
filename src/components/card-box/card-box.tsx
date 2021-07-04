a562f58b67d2e9ad21a62b77b6b45c25ef4db507
Bruno Campos <bhncampos@gmail.com>
2021-07-04 18:20:50 -0300

    Added aws s3 images

--- src/components/card-box/card-box.tsx
+++ src/components/card-box/card-box.tsx
@@ -4,7 +4,8 @@ import { IpfsCardDetails } from '../../services/ipfs/models'
 
 export default function CardBox(props: any) {
   const card = props.card as IpfsCardDetails || new IpfsCardDetails(getPlaceholderCard(), '{CID}');
-  const cardImage = card.Id ? `/images/${card.Id}.jpg` : '/loader.svg';
+  const url = 'https://yugiohimg.s3.sa-east-1.amazonaws.com';
+  const cardImage = card.Id ? `${url}/images/${card.Id}.jpg` : '/loader.svg';
     
   return(
     <Disclosure onClick={props.onClick} key={card.Id} as="div" className="w-full px-2 my-2 cursor-pointer	">
